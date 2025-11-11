8542df20133f944d17b4c3e6e34b16daacab13f3
HelloPaul <Paulpedrano@gmail.com>
2025-11-11 16:54:19 +0800

    Set up Spring Boot app and configure MySQL; tables created

---
+++ src/main/java/com/movie/movieApi/repositories/MovieRepository.java
@@ -0,0 +1,7 @@
+package com.movie.movieApi.repositories;
+
+import com.movie.movieApi.entities.Movie;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MovieRepository extends JpaRepository<Movie, Integer> {
+}
